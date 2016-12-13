6e8172873035aa6147aa26bcbd8b07740d484458
zhaiyaqi <zyqlskq@163.com>
2016-12-13 16:17:50 +0800

    yes

---
+++ src/weaverjn/qlzy/sap/browser/Equipment2OABrowser.java
@@ -0,0 +1,216 @@
+package weaverjn.qlzy.sap.browser;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import weaver.conn.RecordSet;
+import weaver.conn.RecordSetTrans;
+import weaver.general.BaseBean;
+import weaverjn.qlzy.sap.WSClientUtils;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * Created by zhaiyaqi on 2016/11/30.
+ */
+public class Equipment2OABrowser extends BaseBean {
+    public String run(String EQUNR, String company, String EQKTX, String HERST, String TYPBZ, String KOSTL, String TIDNR) {
+        String datas = "";
+        String WERKS = "";
+        if (EQUNR.isEmpty()) {
+            EQUNR = "*";
+        }
+        if (EQKTX.isEmpty()) {
+            EQKTX = "*";
+        }
+        if (HERST.isEmpty()) {
+            HERST = "*";
+        }
+        if (TYPBZ.isEmpty()) {
+            TYPBZ = "*";
+        }
+        if (KOSTL.isEmpty()) {
+            KOSTL = "*";
+        }
+        if (TIDNR.isEmpty()) {
+            TIDNR = "*";
+        }
+        if(company.equals("63")){
+            WERKS = "1010";
+            datas = getDatas(EQUNR, WERKS, EQKTX, HERST, TYPBZ, KOSTL, TIDNR);
+        }else if(company.equals("62")){
+            WERKS = "1030";
+            datas = getDatas(EQUNR, WERKS, EQKTX, HERST, TYPBZ, KOSTL, TIDNR);
+        }else if(company.equals("143")){
+            WERKS = "1060";
+            datas = getDatas(EQUNR, WERKS, EQKTX, HERST, TYPBZ, KOSTL, TIDNR);
+        }else if(company.equals("121")){
+            WERKS = "1070";
+            datas = getDatas(EQUNR, WERKS, EQKTX, HERST, TYPBZ, KOSTL, TIDNR);
+        }else if(company.equals("142")){
+            WERKS = "1630";
+            datas = getDatas(EQUNR, WERKS, EQKTX, HERST, TYPBZ, KOSTL, TIDNR);
+        } else if (company.equals("61")) {
+            WERKS = "1610";
+            String s1 = getDatas(EQUNR, WERKS, EQKTX, HERST, TYPBZ, KOSTL, TIDNR);
+            WERKS = "1620";
+            String s2 = getDatas(EQUNR, WERKS, EQKTX, HERST, TYPBZ, KOSTL, TIDNR);
+            datas = s1.replace("</list>", "") + s2.replace("<list>", "");
+        }
+        return datas;
+    }
+
+    private String getDatas(String EQUNR, String WERKS, String EQKTX, String HERST, String TYPBZ, String KOSTL, String TIDNR) {
+        String datas;
+        String request = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:erp=\"http://qilu-pharma.com.cn/ERP01/\">\n" +
+                "   <soapenv:Header/>\n" +
+                "   <soapenv:Body>\n" +
+                "      <erp:MT_Equi_Par>\n" +
+                "         <EQUNR>" + EQUNR + "</EQUNR>\n" +
+                "         <WERKS>" + WERKS + "</WERKS>\n" +
+                "         <EQKTX>" + EQKTX + "</EQKTX>\n" +
+                "         <HERST>" + HERST + "</HERST>\n" +
+                "         <TYPBZ>" + TYPBZ + "</TYPBZ>\n" +
+                "         <KOSTL>" + KOSTL + "</KOSTL>\n" +
+                "         <TIDNR>" + TIDNR + "</TIDNR>\n" +
+                "      </erp:MT_Equi_Par>\n" +
+                "   </soapenv:Body>\n" +
+                "</soapenv:Envelope>";
+        HashMap<String, String> httpHeaderParm = new HashMap<String, String>();
+        String url = "http://podev.qilu-pharma.com:50000/XISOAPAdapter/MessageServlet?senderParty=&senderService=BS_OADEV&receiverParty=&receiverService=&interface=SI_Equi2OA_Out&interfaceNamespace=http://qilu-pharma.com.cn/ERP01/";
+        httpHeaderParm.put("instId", "10062");
+        httpHeaderParm.put("repairType", "RP");
+        String response = WSClientUtils.callWebServiceWithHttpHeaderParm(request, url, httpHeaderParm);
+        datas = parseData(response);
+//        log(datas);
+        return datas;
+    }
+    private String parseData(String string) {
+        StringBuffer s = new StringBuffer();
+        s.append("<list>");
+        try {
+            Document dom = DocumentHelper.parseText(string);
+            Element root = dom.getRootElement();
+            Element MT_Equi2OA = root.element("Body").element("MT_Equi2OA");
+            Element MSG = MT_Equi2OA.element("MSG");
+            String MSG_TYPE = MSG.elementText("MSG_TYPE");
+            String MESSAGE = MSG.elementText("MESSAGE");
+            if ("S".equals(MSG_TYPE) && "查询成功".equals(MESSAGE)) {
+                Iterator iterator = MT_Equi2OA.elementIterator("Equi_List");
+                ArrayList<String> sqls = new ArrayList<String>();
+                sqls.add("delete from Equipment2OA");
+                while (iterator.hasNext()) {
+                    Element e = (Element) iterator.next();
+                    s.append("<bean>");
+                    s.append("<EQUNR>").append(e.elementText("EQUNR")).append("</EQUNR>");
+                    s.append("<EQKTX>").append(e.elementText("EQKTX")).append("</EQKTX>");
+                    s.append("<EQART>").append(e.elementText("EQART")).append("</EQART>");
+                    s.append("<TYPBZ>").append(e.elementText("TYPBZ")).append("</TYPBZ>");
+                    s.append("<INBDT>").append(e.elementText("INBDT")).append("</INBDT>");
+                    s.append("<TPLNR>").append(e.elementText("TPLNR")).append("</TPLNR>");
+                    s.append("<HERST>").append(e.elementText("HERST")).append("</HERST>");
+                    s.append("<TIDNR>").append(e.elementText("TIDNR")).append("</TIDNR>");
+                    s.append("<KOSTL>").append(e.elementText("KOSTL")).append("</KOSTL>");
+                    s.append("<ANLNR>").append(e.elementText("ANLNR")).append("</ANLNR>");
+                    s.append("<ANLN2>").append(e.elementText("ANLN2")).append("</ANLN2>");
+                    s.append("<SRC_VALUE>").append(e.elementText("SRC_VALUE")).append("</SRC_VALUE>");
+                    s.append("<NET_VALUE>").append(e.elementText("NET_VALUE")).append("</NET_VALUE>");
+                    s.append("</bean>");
+                    String a = getsql(
+                            e.elementText("EQUNR"),
+                            e.elementText("EQKTX"),
+                            e.elementText("EQART"),
+                            e.elementText("TYPBZ"),
+                            e.elementText("INBDT"),
+                            e.elementText("TPLNR"),
+                            e.elementText("HERST"),
+                            e.elementText("TIDNR"),
+                            e.elementText("KOSTL"),
+                            e.elementText("ANLNR"),
+                            e.elementText("ANLN2"),
+                            e.elementText("SRC_VALUE"),
+                            e.elementText("NET_VALUE")
+                    );
+                    sqls.add(a);
+                }
+                exesql(sqls);
+            }
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        s.append("</list>");
+        return s.toString();
+    }
+
+    private void log(Object o) {
+        writeLog(o);
+        System.out.println(o);
+    }
+
+    private void exesql(ArrayList<String> arrayList) {
+        String driver = "oracle.jdbc.driver.OracleDriver";
+        String username = "ecology";
+        String password = "ecology";
+        String url = "jdbc:oracle:thin:@192.168.1.109:1521:ecology";
+        Connection conn = null;
+        Statement statement = null;
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+            statement = conn.createStatement();
+            for (String s : arrayList) {
+                statement.addBatch(s);
+            }
+            statement.executeBatch();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private String getsql(String EQUNR, String EQKTX, String EQART, String TYPBZ, String INBDT, String TPLNR,
+                           String HERST, String TIDNR, String KOSTL, String ANLNR, String ANLN2, String SRC_VALUE, String NET_VALUE) {
+
+        return "insert into Equipment2OA(EQUNR,EQKTX,EQART,TYPBZ,INBDT,TPLNR,HERST,TIDNR,KOSTL,ANLNR,ANLN2,SRC_VALUE,NET_VALUE) values(" +
+                "'" + EQUNR + "'," +
+                "'" + EQKTX + "'," +
+                "'" + EQART + "'," +
+                "'" + TYPBZ + "'," +
+                "'" + INBDT + "'," +
+                "'" + TPLNR + "'," +
+                "'" + HERST + "'," +
+                "'" + TIDNR + "'," +
+                "'" + KOSTL + "'," +
+                "'" + ANLNR + "'," +
+                "'" + ANLN2 + "'," +
+                "'" + SRC_VALUE + "'," +
+                "'" + NET_VALUE + "'" +
+                ")";
+    }
+
+    public static void main(String[] args) {
+        Equipment2OABrowser t = new Equipment2OABrowser();
+        String s = t.run("", "63", "", "", "", "", "");
+        System.out.println(s);
+    }
+}
---
+++ src/weaverjn/qlzy/sap/browser/OAMaterialSearchBrowser.java
@@ -0,0 +1,171 @@
+package weaverjn.qlzy.sap.browser;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import weaver.conn.RecordSet;
+import weaver.conn.RecordSetTrans;
+import weaver.general.BaseBean;
+import weaverjn.qlzy.sap.WSClientUtils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * Created by zhaiyaqi on 2016/12/1.
+ */
+public class OAMaterialSearchBrowser extends BaseBean {
+    public String run(String Company_Code, String MATNR, String MAKTX, String company, String MATKL) {
+        String datas = "";
+        String WERKS = "";
+        if(company.equals("63")){
+            WERKS = "1010";
+            datas = getDatas(Company_Code, MATNR, MAKTX, WERKS, MATKL);
+        }else if(company.equals("62")){
+            WERKS = "1030";
+            datas = getDatas(Company_Code, MATNR, MAKTX, WERKS, MATKL);
+        }else if(company.equals("143")){
+            WERKS = "1060";
+            datas = getDatas(Company_Code, MATNR, MAKTX, WERKS, MATKL);
+        }else if(company.equals("121")){
+            WERKS = "1070";
+            datas = getDatas(Company_Code, MATNR, MAKTX, WERKS, MATKL);
+        }else if(company.equals("142")){
+            WERKS = "1630";
+            datas = getDatas(Company_Code, MATNR, MAKTX, WERKS, MATKL);
+        } else if (company.equals("61")) {
+            WERKS = "1610";
+            String s1 = getDatas(Company_Code, MATNR, MAKTX, WERKS, MATKL);
+            WERKS = "1620";
+            String s2 = getDatas(Company_Code, MATNR, MAKTX, WERKS, MATKL);
+            datas = s1.replace("</list>", "") + s2.replace("<list>", "");
+        }
+        return datas;
+    }
+
+    private String getDatas(String Company_Code, String MATNR, String MAKTX, String WERKS, String MATKL) {
+        String request = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:erp=\"http://qilu-pharma.com.cn/ERP01/\">\n" +
+                "   <soapenv:Header/>\n" +
+                "   <soapenv:Body>\n" +
+                "      <erp:MT_OAMaterialSearch>\n" +
+                "         <ControlInfo>\n" +
+                "            <INTF_ID>I0031</INTF_ID>\n" +
+                "            <Src_System>OA</Src_System>\n" +
+                "            <Dest_System>SAPERP</Dest_System>\n" +
+                "            <Company_Code>" + Company_Code + "</Company_Code>\n" +
+                "            <Send_Time>20161201131310</Send_Time>\n" +
+                "         </ControlInfo>\n" +
+                "         <Search_Condition>\n" +
+                "            <MATNR>" + MATNR + "</MATNR>\n" +
+                "            <MAKTX>" + MAKTX + "</MAKTX>\n" +
+                "            <WERKS>" + WERKS + "</WERKS>\n" +
+                "            <MATKL>" + MATKL + "</MATKL>\n" +
+                "         </Search_Condition>\n" +
+                "      </erp:MT_OAMaterialSearch>\n" +
+                "   </soapenv:Body>\n" +
+                "</soapenv:Envelope>";
+        HashMap<String, String> httpHeaderParm = new HashMap<String, String>();
+        String url = "http://podev.qilu-pharma.com:50000/XISOAPAdapter/MessageServlet?senderParty=&senderService=BS_OADEV&receiverParty=&receiverService=&interface=SI_OAMaterialSearch_Out&interfaceNamespace=http://qilu-pharma.com.cn/ERP01/";
+        httpHeaderParm.put("instId", "10062");
+        httpHeaderParm.put("repairType", "RP");
+        String response = WSClientUtils.callWebServiceWithHttpHeaderParm(request, url, httpHeaderParm);
+        String datas = parseData(response);
+        datas = datas.replaceAll("&", "&amp;");
+//        log(datas);
+        return datas;
+    }
+
+    private String parseData(String string) {
+        StringBuilder s = new StringBuilder();
+        s.append("<list>");
+        try {
+            Document dom = DocumentHelper.parseText(string);
+            Element root = dom.getRootElement();
+            Element MT_Mat_Data2OA = root.element("Body").element("MT_Mat_Data2OA");
+
+            Iterator iterator = MT_Mat_Data2OA.elementIterator("Mat_Data");
+            ArrayList<String> sqls = new ArrayList<String>();
+            sqls.add("delete from OAMaterialSearch");
+            while (iterator.hasNext()) {
+                Element e = (Element) iterator.next();
+                s.append("<bean>");
+                s.append("<MATNR>").append(e.elementText("MATNR")).append("</MATNR>");
+                s.append("<MAKTX>").append(e.elementText("MAKTX")).append("</MAKTX>");
+                s.append("<MEINS>").append(e.elementText("MEINS")).append("</MEINS>");
+                s.append("<WERKS>").append(e.elementText("WERKS")).append("</WERKS>");
+                s.append("<MATKL>").append(e.elementText("MATKL")).append("</MATKL>");
+                s.append("</bean>");
+                sqls.add(getsql(e.elementText("MATNR"), e.elementText("MAKTX"), e.elementText("MEINS"), e.elementText("WERKS"), e.elementText("MATKL")));
+            }
+            exesql(sqls);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        s.append("</list>");
+        return s.toString();
+    }
+
+    private void log(Object o) {
+        writeLog(o);
+        System.out.println(o);
+    }
+
+    private void exesql(ArrayList<String> arrayList) {
+        String driver = "oracle.jdbc.driver.OracleDriver";
+        String username = "ecology";
+        String password = "ecology";
+        String url = "jdbc:oracle:thin:@192.168.1.109:1521:ecology";
+        Connection conn = null;
+        Statement statement = null;
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+            statement = conn.createStatement();
+            for (String s : arrayList) {
+                statement.addBatch(s);
+            }
+            statement.executeBatch();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private String getsql(String MATNR, String MAKTX, String MEINS, String WERKS, String MATKL) {
+        return "insert into OAMaterialSearch(MATNR,MAKTX,MEINS,WERKS,MATKL) values(" +
+                "'" + MATNR + "'," +
+                "'" + MAKTX + "'," +
+                "'" + MEINS + "'," +
+                "'" + WERKS + "'," +
+                "'" + MATKL + "'" +
+                ")";
+    }
+
+    public static void main(String[] args) {
+        OAMaterialSearchBrowser t = new OAMaterialSearchBrowser();
+        String s = t.run("", "", "", "63", "");
+        System.out.println(s);
+    }
+}
