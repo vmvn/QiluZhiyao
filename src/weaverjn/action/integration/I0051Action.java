26a8d41af564a759735ed0881dc2b363c676e3f7
zhaiyaqi <zyqlskq@163.com>
2017-02-09 11:54:11 +0800

    添加I0051接口action源码

---
+++ src/weaverjn/action/integration/I0051Action.java
@@ -0,0 +1,16 @@
+package weaverjn.action.integration;
+
+import weaver.general.BaseBean;
+import weaver.interfaces.workflow.action.Action;
+import weaver.soa.workflow.request.RequestInfo;
+
+/**
+ * Created by zhaiyaqi on 2017/2/9.
+ */
+public class I0051Action extends BaseBean implements Action {
+
+    @Override
+    public String execute(RequestInfo requestInfo) {
+        return null;
+    }
+}
